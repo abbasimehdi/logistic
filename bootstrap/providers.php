16b046a450536964f3d4cfeb3283be2589956fb4
m.abbasi <abbasimehdi24@gmail.com>
2025-11-16 19:27:51 +0330

    Add modules provides

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,6 +1,10 @@
 <?php
 
+
 return [
     App\Providers\AppServiceProvider::class,
-    \App\Modules\Domain\Core\CoreServiceProvider::class
+    \Logistic\Modules\Domain\Core\CoreServiceProvider::class,
+    \Logistic\Modules\Domain\Driver\DriverServiceProvider::class,
+    \Logistic\Modules\Domain\Vehicle\VehicleServiceProvider::class,
+    \Logistic\Modules\Domain\Assignment\AssignmentServiceProvider::class,
 ];
